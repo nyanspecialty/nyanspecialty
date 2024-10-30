24701afd91cfbf04dc0488d66cac788f7a089c0e
Durga Prasad Bgahuthu <dbaghuthu@betalen.in>
2024-10-30 17:54:53 +0530

    commit

---
+++ NyanSpecialty.Assistance.Web.UI/src/app/policytype/policytype.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policytype',
+  standalone: true,
+  imports: [],
+  templateUrl: './policytype.component.html',
+  styleUrl: './policytype.component.css'
+})
+export class PolicytypeComponent {
+
+}
